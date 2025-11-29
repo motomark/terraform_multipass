a14b53e2791a1212fa6f849eb8ce72b2912e6e60
Mark H <markhawkins@blueyonder.co.uk>
2025-11-29 17:10:46 +0000

    Basic VM on Multipass with SSH

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # terraform_multipass
 Example Terraform and Ansible on multipass hypervisor
+
+Add the Mac’s public key to the VM
+SSH into the multipass vm using your normal ~/.ssh/id_rsa :
+
+1. Run multipass shell <vm-name> to get a shell inside the VM.
+
+2. Append your Mac’s public key into the VM’s ~/.ssh/authorized_keys, e.g.:
+
+```cat ~/.ssh/id_rsa.pub | pbcopy      # on Mac  
+multipass shell <vm-host>  
+echo "<paste-the-pub-key-here>" >> ~/.ssh/authorized_keys  
+chmod 600 ~/.ssh/authorized_keys  
+exit  
+```
+
+
+Then you can SSH from your Mac without pointing to Multipass’s root-owned key. 
+```
+ssh ubuntu@<vm-host>
+```
---
+++ basic/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    multipass = {
+      source  = "todoroff/multipass"
+      version = ">= 1.2.0"
+    }
+  }
+}
+
+provider "multipass" {
+  default_image = "lts"
+}
+
+data "multipass_images" "lts" {
+  alias = "lts"
+}
+
+resource "multipass_instance" "dev" {
+  name   = "dev-box"
+  image  = data.multipass_images.lts.images[0].name
+  cpus   = 2
+  memory = "4G"
+  disk   = "15G"
+
+  networks {
+    name = "en0"
+  }
+}
+
+resource "multipass_alias" "shell" {
+  name     = "dev-shell"
+  instance = multipass_instance.dev.name
+  command  = "bash"
+}
+
+output "dev_ip" {
+  value = multipass_instance.dev.ipv4
+}
+
